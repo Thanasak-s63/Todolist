8ad9329e433bdb3a4da3de979131142c2f471c92
Sattapoom Tulyasuk <s6301012630192@kmutnb.ac.th>
2022-01-30 20:48:44 +0700

    Create server.js

---
+++ README.md
@@ -0,0 +1,3 @@
+# Commit 1 : Create server.js
+
+สร้างไฟล์ server.js เพื่อใช้จัดการกับส่วน backend โดยใช้ express.js เพื่อใช้งาน middleware โดยสร้าง app เป็นตัวแปร instance ของ express ซึ่งสามารถเรียกใช้ use ในการเรียกใช้ middleware อื่น , get ในการรับ get request ของ url ที่เรากำหนดโดย ตัวฟังก์ชั่นที่ใช้ใน get ก็เป็น middleware เรียกว่า middleware function โดยสามารถจัดการ response และ request ได้ , listen ใช้ในการรับการเชื่อมต่อใน port นั้นๆ
---
+++ backend/server.js
@@ -0,0 +1,29 @@
+const express = require("express"); //import express for build REST api => DELETE,PUT,GET,POST,etc.*
+const bodyParser = require("body-parser");
+const cors = require("cors"); //import cross-origin-resource Sharing -> file*
+
+const app = express();
+
+var corsOptions = {
+    origin: "*"
+};
+
+
+app.use(cors(corsOptions));
+
+app.use(bodyParser.json()); // request type json express send json
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//simple route
+app.get("/", (req, res) => { // req = request  res = respone
+    res.json({ message: "Welcome to TodoList Application." });
+})
+
+require("./app/routes/todo.routes")(app);
+
+//set port
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}.`);
+})
