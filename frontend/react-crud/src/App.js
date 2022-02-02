7a1b1aac30602c67aa0d31900f3096fb1c556763
Wachirawit-s63 <s6301012630150@email.kmutnb.ac.th>
2022-02-02 20:59:12 +0700

    Create routing system by using react router

--- frontend/react-crud/src/App.js
+++ frontend/react-crud/src/App.js
@@ -1,10 +1,37 @@
-import './App.css';
+import React, { Component } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom';
+import "./App.css";
 
 function App() {
   return (
-    <div>
+    <Router>
+        <div>
+          <nav className="navbar navbar-expand navbar-dark bg-dark">
+            <div className="navbar-nav mr-auto">
+              <li className="nav-item">
+                <Link to={"/todo"} className="nav-link">
+                  TODO-LIST
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link to={"/add"} className="nav-link">
+                  Add
+                </Link>
+              </li>
+            </div>
+          </nav>
 
-    </div>
+          <div className="container mt-3">
+            <Switch>
+              <Route exact path={["/", "/todo"]} component={TodoList} />
+              <Route exact path="/add" component={AddTodo} />
+              <Route path="/todo/:id" component={Todo} />
+            </Switch>
+          </div>
+        </div>
+      </Router>
+      
   );
 }
 
